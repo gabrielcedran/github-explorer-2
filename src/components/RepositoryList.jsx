8e97e61260e1bec69db7c674adc6a2382ae1a2dc
Ga Cdn <gabrielcedran@gmail.com>
2021-12-05 16:35:38 +0000

    Componentising base element to enable reuse and avoid repetition

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+import { RepositoryItem } from "./RepositoryItem";
 
 export function RepositoryList() {
     return (
@@ -5,20 +6,8 @@ export function RepositoryList() {
             <h1>Repository List</h1>
 
             <ul>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">
-                        Open Repository
-                    </a>
-                </li>
-                <li>
-                    <strong>unform</strong>
-                    <p>Forms in React</p>
-                    <a href="">
-                        Open Repository
-                    </a>
-                </li>
+                <RepositoryItem repository="test" />
+                <RepositoryItem repository="test 2" />
             </ul>
         </section>
     )
